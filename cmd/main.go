b7158fb79521374f4f59734169d0be032adc1c16
zc <zc2638@qq.com>
2020-09-23 14:59:08 +0800

    remove load time location

--- cmd/main.go
+++ cmd/main.go
@@ -10,17 +10,10 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/zc2638/drone-control/cmd/app"
 	"os"
-	"time"
 	_ "time/tzdata"
 )
 
 func main() {
-	location, err := time.LoadLocation("Asia/Shanghai")
-	if err != nil {
-		fmt.Println("load location Asia/Shanghai error: ", err)
-	} else {
-		time.Local = location
-	}
 	command := app.NewServerCommand()
 	if err := command.Execute(); err != nil {
 		fmt.Println(err)
